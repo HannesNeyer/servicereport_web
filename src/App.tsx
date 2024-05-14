9c13dfcb15405ec19b41eb20ee4198edb6b51539
Alexander II <alexander.tschanun@icloud.com>
2024-05-14 09:17:42 +0200

    14.05.2024_Letter-Init

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LoginPage from './components/LoginPage'
+import LetterOfAcceptance from './components/LetterOfAcceptance'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <LoginPage />
-    </>
+    <LetterOfAcceptance/>
   )
 }
 
---
+++ src/components/LetterOfAcceptance.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function LetterOfAcceptance({ }: Props) {
+    return (
+        <div className='bg-green-400'>Letter</div>
+    )
+}
