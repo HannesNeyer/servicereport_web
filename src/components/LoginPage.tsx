0b0e4e2ff0aed0a56dda84e89223df28fb9dc11e
HannesNeyer <hannes.neyer@student.htldornbirn.at>
2024-05-16 14:48:32 +0200

    LoginPageDesign1

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -4,17 +4,23 @@ type Props = {}
 
 export default function LoginPage({ }: Props) {
     return (
-        <div className='relative w-[24.3rem]'>
-            <div className='flex'>
-                <img src="sonderhofLogo.png" className='mt-2 w-18 h-12'></img>
-                <h1 className='ml-2 font-bold text-5xl font-sans text-[#029294] z-10'>ServiceReport</h1>
+        <div>
+            <div className='relative w-[23.8rem] ml-96 mt-48'>
+                <div className='flex'>
+                    <img src="sonderhofLogo.png" className='mt-2 w-18 h-12'></img>
+                    <h1 className='font-bold text-5xl font-sans text-[#029294] z-10'>ServiceReport</h1>
+                </div>
+
+                <div className='absolute mt-[-0.5rem] right-0 h-72 w-[21rem] border-[0.25rem] rounded-lg border-gray-500'>
+                    <p className='font-light fon-sans text-5xl text-[#029294] mb-5 ml-2'>Login</p>
+                    <div className='h-10 w-[20.2rem] border-[0.25rem] rounded-lg border-gray-500 mb-3 ml-2'></div>
+                    <p className='font-light fon-sans text-5xl text-[#029294] mb-3 ml-2'>Passwort</p>
+                    <div className='h-10 w-[20.2rem] border-[0.25rem] rounded-lg border-gray-500 ml-2'></div>
+                </div>
             </div>
 
-            <div className='absolute mt-[-0.5rem] right-0 h-72 w-[21rem] border-[0.25rem] rounded-lg border-gray-500'>
-                <p className='font-light fon-sans text-5xl text-[#029294] mb-5 ml-2'>Login</p>
-                <div className='h-10 w-[21.2rem] border-[0.25rem] rounded-lg border-gray-500 mb-3 ml-2'></div>
-                <p className='font-light fon-sans text-5xl text-[#029294] mb-3 ml-2'>Passwort</p>
-                <div className='h-10 w-[21.2rem] border-[0.25rem] rounded-lg border-gray-500 ml-2'></div>
+            <div className=''>
+                <img src="henkelSonderhofLogo.png" className='mt-2 w-96 h-38'></img>
             </div>
         </div>
     )
