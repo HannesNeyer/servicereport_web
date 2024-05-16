dcd3431d3812d8e73ec3dfeda16a33ab195bef95
HannesNeyer <hannes.neyer@student.htldornbirn.at>
2024-05-16 14:23:49 +0200

    login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import LoginPage from './components/LoginPage'
+import LoginPage from './components/LoginPage.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <LoginPage />
+      <LoginPage/>
     </>
   )
 }
