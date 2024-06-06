82324787830f12e8c045691e7d32ef62ac78c324
Alexander II <alexander.tschanun@icloud.com>
2024-06-06 13:51:20 +0200

    06.06.2024_LoA-Prototype

--- src/components/LetterOfAcceptance.tsx
+++ src/components/LetterOfAcceptance.tsx
@@ -4,6 +4,22 @@ type Props = {}
 
 export default function LetterOfAcceptance({ }: Props) {
     return (
-        <div className='bg-green-400'>Letter</div>
+        <div>
+            <div className='text-[#029294]'>Allgemein</div>
+            <div className='text-[#029294]'>gem. ihrer Bestllung</div>
+
+            <div>
+                <div  className='border-2 border-gray-500'>1. Lieferung gemäß ihrem Auftrag vollständig und unbeschädigt erhalten</div>
+                <input type="checkbox"></input>
+                <input type="checkbox"></input>
+
+                <div className=' border-2 border-gray-500'>
+                    <div className='border-2'>Bemerkungen</div>
+                    <input type="text" className='border-2'></input>
+                </div>
+            </div>
+
+        </div>
+        
     )
 }
